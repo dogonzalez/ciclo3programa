b6a68984d529d528ec05d6fb85a612d384c25d91
oscaravid <oscaravid@outlook.com>
2021-09-21 19:07:21 -0500

    cambio

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -2,9 +2,8 @@ public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados() {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
-        miVertebrados[3] = new Vertebrado("Perro", "Mamifero");
+        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");        
+        miVertebrados[2] = new Vertebrado("Perro", "Mamifero");
 
         return miVertebrados;
     }
